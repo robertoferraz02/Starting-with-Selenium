8ed0d8c92f7b0c1aaceaf204eedfe08b9bee9776
Roberto Ferraz <roberto_ferraz15@hotmail.com>
2018-07-01 20:49:20 -0300

    Trying to discover how to work with Selenium

--- FirstSeleniumScript.py
+++ FirstSeleniumScript.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from time import sleep as sleep
 
 driver = webdriver.Chrome()
 driver.maximize_window()
